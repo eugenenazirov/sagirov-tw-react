fe1fae896f5b237e61dc6edc2d596c77f092acba
Eugene Nazirov <jagzonmusic@gmail.com>
2022-11-02 15:34:50 +0600

    Initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,47 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <header className="header first-screen__header">
+            <div className="container header__container">
+                <a href="#" className="header__logo-block">
+                    <div className="header__corner header__corner_top-left">
+                        <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 8V1H8" stroke="white"/>
+                        </svg>
+                    </div>
+                    <div className="header__corner header__corner_bottom-left">
+                        <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 0V7H8" stroke="white"/>
+                        </svg>
+                    </div>
+                    <div className="header__corner header__corner_top-right">
+                        <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M7 8V1H0" stroke="white"/>
+                        </svg>
+                    </div>
+                    <div className="header__corner header__corner_bottom-right">
+                        <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M7 0V7H0" stroke="white"/>
+                        </svg>
+                    </div>
+                    <div ref="#" className="logo__image">
+                        <img className="logo__image-img" src="../../images/SpaceX-Logo.svg" alt="Логотип SpaceX" />
+                    </div>
+                </a>
+                <nav className="menu">
+                    <ul className="menu__items">
+                        <li><a className="menu__item" ref="#main">Главная</a></li>
+                        <li><a className="menu__item" ref="#tech">Технология</a></li>
+                        <li><a className="menu__item" ref="#schedule">График полетов</a></li>
+                        <li><a className="menu__item" ref="#guarantee">Гарантии</a></li>
+                        <li><a className="menu__item" ref="#about">О компании</a></li>
+                        <li><a className="menu__item" ref="#contacts">Контакты</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,9 @@
+import './HeroSection.css';
+
+function HeroSection() {
+    return (
+        <div className="hero">Hero Section!</div>
+    );
+}
+
+export default HeroSection;
